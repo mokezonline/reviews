67ea69438d5392767a0c7c6a7947a50ad2afc048
Bjorklundsamm <bjorklundsamm@gmail.com>
2021-01-14 10:07:54 -0800

    Added express features and connected server with DB

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-const
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT  = 3000;
+
+
+const productDB  = require ('../database/Product.js');
+const reviewDB  = require('../database/Review.js');
+
+const productRoutes =  require('./routes/productR');
+const reviewRoutes = require('./routes/reviewR');
+
+app.use('/', express.static(path.join('dirname', '..', 'client', 'src')))
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+})
